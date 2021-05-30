b2966a26c0cecd23a168870014261a12f5ced3fa
PranjayPathak <pranjaypathak411@gmail.com>
2021-05-30 14:36:30 +0530

    setup 'parcel' and 'babel(core/preset-react)'

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./pet.js";
 
 const App = () => {
-    return( React.createElement(
-        "div",
-        {}, 
-        [
-            React.createElement("h1",{},"Adopt me"),
-            React.createElement(Pet,{name:"Buzey" , animal:"Dog", Breed:"Indian Pariah" }),
-            React.createElement(Pet,{name:"Buzey" , animal:"Dog", Breed:"Indian Pariah" }),
-            React.createElement(Pet,{name:"Buzey" , animal:"Dog", Breed:"Indian Pariah" })
-        ]
-    ));
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me"),
+    React.createElement(Pet, {
+      name: "Buzey",
+      animal: "Dog",
+      Breed: "Indian Pariah",
+    }),
+    React.createElement(Pet, {
+      name: "Buzey",
+      animal: "Dog",
+      Breed: "Indian Pariah",
+    }),
+    React.createElement(Pet, {
+      name: "Buzey",
+      animal: "Dog",
+      Breed: "Indian Pariah",
+    }),
+  ]);
 };
 
-const Pet = (props) => {
-    return React.createElement(
-        "div",
-        {},
-        [React.createElement("h2",{},props.name),
-        React.createElement("h3",{},props.animal),
-        React.createElement("h3",{},props.brees)],
-        
-    )
-}
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
