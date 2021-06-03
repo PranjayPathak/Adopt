cac6b6ab96544934e5d7a5d4fbcf4d75527a4412
PranjayPathak <pranjaypathak411@gmail.com>
2021-06-03 20:40:56 +0530

    added universal modal, currently only on details page

--- src/details.js
+++ src/details.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 import Carousel from "./carousel";
 import ErrorBoundary from "./errorBoundary";
 import ThemeContext from "./themeContext";
+import Modal from "./modal";
 
 class Details extends Component {
   // constructor() {
@@ -19,12 +20,18 @@ class Details extends Component {
     this.setState(Object.assign({ loading: false }, json.pets[0])); //whole object in state
   }
 
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
+  adopt = () => (window.location = "http://bit.ly/pet-adopt");
+
   render() {
     if (this.state.loading) {
       return <h2>Loading ..</h2>;
     }
     // throw new Error("awdawdw");
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
     return (
       <div className="details">
@@ -34,10 +41,27 @@ class Details extends Component {
           <h2>{`${animal} - ${breed} - ${city},${state}`}</h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No, I'm a monster</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
--- src/modal.js
+++ src/modal.js
@@ -1,4 +1,20 @@
 import { createPortal } from "react-dom";
-import { useEffect, useRef } from "reeact";
+import { useEffect, useRef } from "react";
 
 const modalRoot = document.getElementById("modal");
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+  useEffect(() => {
+    modalRoot.appendChild(elRef.current);
+
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
